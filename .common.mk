f667127036ad0bd6157dfbb6710cfc03e64896b0
westenfalke <github_com_@westenfalke.de>
2021-01-07 23:42:48 +0100

    added SHOWCASE RECIPE
    added SHOWCASE FILES
    
    updates DATE-ISO
    
    added BASH and HASH_BANG
    
    reseted ABS_PROJECT_DIR to tmp

--- .common.mk
+++ .common.mk
@@ -1,7 +1,14 @@
 # VAR
-ABS_PROJECT_DIR         = /home/westenfalke/GNU-Make/CAHTFC
+ABS_PROJECT_DIR         = /tmp
 CHAPTERS                = readme 
+ABS_PROJECT_DIR         = /tmp
 APPENDIX                = index
+FILE_SHOWCASE_PART_01   = 01-showcase-outro.md
+FILE_SHOWCASE_PART_02   = 02-showcase-action.bash
+FILE_SHOWCASE_PART_03   = 03-showcase-outro.md
+RECIPE_SHOWCASE_PART_01 = $(CAT)   $(FILE_SHOWCASE_PART_01) $(PIPE-TEE-A-TO-TARGET)
+RECIPE_SHOWCASE_PART_02 = -$(BASH) $(FILE_SHOWCASE_PART_02) $(PIPE-TEE-A-TO-TARGET)
+RECIPE_SHOWCASE_PART_03 = $(CAT)   $(FILE_SHOWCASE_PART_03) $(PIPE-TEE-A-TO-TARGET)
 OUTPUT_DIR              = out
 OUTPUT_FILE             = README.md
 PHONY_TARGETS           = .phony-targets.mk
@@ -9,9 +16,11 @@ UPDATE_MAKEFILE         = .update-makefiles.mk
 HELP_TARGET             = .help-target.mk
 DEFAULT_TARGET          = .default-target.mk
 # CMD
+BASH                    = /usr/bin/bash
 CAT                     = /usr/bin/cat
 ECHO                    = /usr/bin/echo
 ECHO-N                  = /usr/bin/echo -n
+HASH-BANG               = #!/usr/bin/bash
 LS                      = /usr/bin/ls
 MKDIR-P                 = /usr/bin/mkdir -p
 MOVE--VERBOSE           = /usr/bin/mv --verbose
@@ -29,4 +38,5 @@ TR                      = /usr/bin/tr
 TRUNCATE-ZERO-CREATE    = /usr/bin/truncate --size=0
 # MACRO                 
 PIPE-TEE-A-TO-TARGET    = | /usr/bin/tee -a $@
-ISO-DATE-NANOSEC        = `/usr/bin/date --iso-8601=ns`
+DATE-ISO-SEC            = `/usr/bin/date --iso-8601=second`
+DATE-ISO-NANOSEC        = `/usr/bin/date --iso-8601=ns`
